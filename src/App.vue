93091d46264c833f63019c56dcb2daa4343e52fb
dhirtiman <dhirtimankhati@gmail.com>
2024-02-28 04:35:41 +0530

    done i gues

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,116 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container">
+    <h1>Todos</h1>
+    <br>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <ul class="todo-list">
+      <li v-for="todo in todos" :key="todo.id" @click="doneTask(todo)" :class="{ done: todo.done }">
+        {{ todo.text }}
+        <button class="remove-btn" @click="removeTask(todo)">X</button>
+      </li>
+    </ul>
+
+    <div class="input-container">
+      <input type="text" v-model="todoText" placeholder="Add new task" @keyup.enter="addTask">
+      <button class="add-btn" @click="addTask">Add</button>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
+<script >
+let id = 1;
+
+export default {
+  data() {
+    return {
+      todoText: null,
+      todos: [
+        {
+          id: id++,
+          text: 'Learn Vue',
+          done: false,
+        },
+
+      ],
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.todoText) {
+        const todo = {
+          id: id++,
+          text: this.todoText,
+          done: false,
+        }
+        this.todos.push(todo)
+        this.todoText = null;
+      }
+    },
+    doneTask(todo) {
+      todo.done = !todo.done;
+    },
+    removeTask(todo) {
+      this.todos = this.todos.filter((t) => t !== todo)
+    }
+  },
+}
+
+
+</script>
 <style scoped>
-header {
-  line-height: 1.5;
+.container {
+  max-width: 400px;
+  margin: auto;
+  padding: 20px;
+  transform: translateY(15vh);
+  
+  
+
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.todo-list {
+  list-style-type: none;
+  padding: 0;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.todo-list li {
+  padding: 10px;
+  border-bottom: 1px solid #ddd;
+  cursor: pointer;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.todo-list li.done {
+  text-decoration: line-through;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+
+
+.input-container {
+  margin-top: 20px;
+}
+
+.input-container input {
+  width: 60%;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+
+
+button {
+  background-color: #00000044;
+  border: none;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  padding: 5px 10px;
+  float: right;
+
+
+}
+
+.done {
+  text-decoration: line-through;
 }
 </style>
